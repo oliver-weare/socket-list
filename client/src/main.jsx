d4d3b6cf6c1f8d2de388480c66f78e7b82d67d47
Oliver Weare <oliver_weare@protonmail.com>
2025-02-10 14:09:09 +0000

    Use context in main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App.jsx";
 
+import { TodayListProvider } from "./context/TodayListContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <TodayListProvider>
+      <App />
+    </TodayListProvider>
   </StrictMode>
 );
