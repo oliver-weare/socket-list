bbdc4d2e02788b4ddf244663f3d66dabe4855e10
Oliver Weare <oliver_weare@protonmail.com>
2025-02-10 14:07:36 +0000

    Today's list context component

---
+++ client/src/context/TodayListContext.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useState } from "react";
+
+const TodayListContext = createContext();
+
+export const TodayListProvider = ({ children }) => {
+  const [todayListID, setTodayListID] = useState(null);
+  const [todayListItemNames, setTodayListItemNames] = useState([]);
+  const [todayListExists, setTodayListExists] = useState(false);
+
+  return (
+    <TodayListContext.Provider
+      value={{
+        todayListID,
+        setTodayListID,
+        todayListItemNames,
+        setTodayListItemNames,
+        todayListExists,
+        setTodayListExists,
+      }}
+    >
+      {children}
+    </TodayListContext.Provider>
+  );
+};
+
+export const useTodayList = () => useContext(TodayListContext);
