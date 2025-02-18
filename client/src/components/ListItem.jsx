d29a491a508d60713a0304dbda5e48e21558e3d3
Oliver Weare <oliver_weare@protonmail.com>
2025-02-18 12:37:09 +0000

    Conditional icon rendering

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -6,7 +6,7 @@ import DeleteItemButton from "./buttons/DeleteItemButton";
 import ShiftItemButton from "./buttons/ShiftItemButton";
 
 const ListItem = ({ item, expired }) => {
-  const { todayListID } = useAppContext();
+  const { todayListID, currentCategory } = useAppContext();
 
   const handleTogglePicked = async () => {
     try {
@@ -19,7 +19,11 @@ const ListItem = ({ item, expired }) => {
   };
 
   return (
-    <div className="d-flex align-items-center justify-content-between mb-2">
+    <div
+      className={`d-flex align-items-center justify-content-between mb-2 ${
+        item.category !== currentCategory && "d-none"
+      }`}
+    >
       <button
         className={`toggle-item-button btn d-flex align-items-center rounded-pill ${
           expired && !item.pickedUp ? "btn-warning" : "btn-outline-dark"
@@ -31,20 +35,33 @@ const ListItem = ({ item, expired }) => {
           {item.name}
         </span>
         {item.pickedUp ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-bag-check-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0m-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"
-            />
-          </svg>
-        ) : (
+          item.category === "grocery" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-bag-check-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0m-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-check-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+            </svg>
+          )
+        ) : item.category === "grocery" ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -54,6 +71,17 @@ const ListItem = ({ item, expired }) => {
             viewBox="0 0 16 16"
           >
             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-circle"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
           </svg>
         )}
       </button>
